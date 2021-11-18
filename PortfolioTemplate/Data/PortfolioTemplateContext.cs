7d9bcc0f41792eeb901228fbc6167d17a4a7c355
William La <55414909+WilliamLa59@users.noreply.github.com>
2021-11-18 00:35:27 -0500

    init commit
    project creation performs CRUD operations and created table for data

---
+++ PortfolioTemplate/Data/PortfolioTemplateContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PortfolioTemplate.Models;
+
+namespace PortfolioTemplate.Data
+{
+    public class PortfolioTemplateContext : DbContext
+    {
+        public PortfolioTemplateContext(DbContextOptions<PortfolioTemplateContext> options)
+            : base(options)
+        {
+        }
+        
+        public DbSet<PortfolioTemplate.Models.Project>? project { get; set; }
+    }
+}
---
+++ PortfolioTemplate/Models/Class.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PortfolioTemplate.Models
+{
+    public class Project
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
