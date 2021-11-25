34bab5a268aded76e987bdab1a8be876f255a1e6
William La <55414909+WilliamLa59@users.noreply.github.com>
2021-11-25 18:49:48 -0500

    Added Project Files

---
+++ PortfolioTemplateV2/Migrations/20211122235731_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortfolioTemplateV2.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Project",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<int>(type: "int", nullable: false),
+                    PhotoPath = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Project", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Project");
+        }
+    }
+}
---
+++ PortfolioTemplateV2/Pages/Projects/Delete.cshtml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PortfolioTemplateV2.Data;
+using PortfolioTemplateV2.Models;
+
+namespace PortfolioTemplateV2.Pages.Projects
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly PortfolioTemplateV2.Data.PortfolioTemplateV2Context _context;
+
+        private readonly IWebHostEnvironment _iweb;
+
+        public DeleteModel(PortfolioTemplateV2.Data.PortfolioTemplateV2Context context, IWebHostEnvironment iweb)
+        {
+            _context = context;
+
+            _iweb = iweb;
+        }
+
+        [BindProperty]
+        public Project Project { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Project = await _context.Project.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Project == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Project = await _context.Project.FindAsync(id);
+
+          
+            string filePath = Path.Combine(_iweb.WebRootPath, "images", Project.PhotoPath);
+            
+
+            if (Project != null)
+            {
+                _context.Project.Remove(Project);
+                System.IO.File.Delete(filePath);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
