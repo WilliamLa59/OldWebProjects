2cc12facadea54bb4d918625c46ec9e0839ac161
William La <55414909+WilliamLa59@users.noreply.github.com>
2021-11-25 18:51:02 -0500

    Added Data Folder

---
+++ PortfolioTemplateV2/Data/PortfolioTemplateV2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PortfolioTemplateV2.Models;
+
+namespace PortfolioTemplateV2.Data
+{
+    public class PortfolioTemplateV2Context : DbContext
+    {
+        public PortfolioTemplateV2Context (DbContextOptions<PortfolioTemplateV2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PortfolioTemplateV2.Models.Project> Project { get; set; }
+    }
+}
